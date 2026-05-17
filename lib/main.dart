8e7165ce667e5b92720c4ad9209b8d85dd040f08
ymhamza93 <y.m.hamza93@gmail.com>
2026-05-17 15:08:17 +0300

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MoviesApp());
+}
+
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }}
