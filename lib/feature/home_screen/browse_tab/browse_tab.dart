94fb6f0c463fac807e7ac189f15ddff7466367f3
Shorouq <shorouqelhawary5@gmail.com>
2026-05-28 13:51:05 +0200

    finish browse tab ui and api integration

--- lib/feature/home_screen/browse_tab/browse_tab.dart
+++ lib/feature/home_screen/browse_tab/browse_tab.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/core/utils/color_manager.dart';
+import 'package:movies_app/feature/home_screen/browse_tab/widgets/genre_item.dart';
+import 'package:movies_app/feature/home_screen/browse_tab/widgets/movie_gridview.dart';
 
-class BrowseTab extends StatelessWidget {
+class BrowseTab extends StatefulWidget {
   const BrowseTab({super.key});
 
   @override
+  State<BrowseTab> createState() => _BrowseTabState();
+}
+
+class _BrowseTabState extends State<BrowseTab> {
+  String selectedGenre = 'Action';
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.amber);
+    return Scaffold(
+      backgroundColor: ColorManager.backgroundBlack,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              GenreItem(
+                selectedGenre: selectedGenre,
+
+                onGenreSelected: (genre) {
+                  setState(() {
+                    selectedGenre = genre;
+                  });
+                },
+              ),
+
+              SizedBox(height: MediaQuery.of(context).size.height * .027),
+
+              MovieGridView(genre: selectedGenre),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
